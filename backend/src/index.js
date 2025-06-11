5df55382d7a902b9241337a18a307788734f44c6
Soumya <jenasoumya2004@gmail.com>
2025-06-11 10:42:46 +0530

    Final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,6 +40,7 @@ app.use("/api/messages", messageRoutes);
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  //StackOverflow OG : "/*" didn't work for me, but "/{*any}" did"
   app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
